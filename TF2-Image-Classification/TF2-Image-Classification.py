352f26a753cc82a0db371e6942fa1189e3d81cd4
Sendeky <spruslasha@gmail.com>
2023-07-18 16:08:46 -0700

    added a bit more to image classification, added folder for transfer learning

--- TF2-Image-Classification/TF2-Image-Classification.py
+++ TF2-Image-Classification/TF2-Image-Classification.py
@@ -156,10 +156,29 @@ history = model.fit(
   epochs=epochs
 )
 
-# much better results, red line is usually val_loss, 
+# much better results
 plt.plot(history.history['accuracy'], label="accuracy")
 plt.plot(history.history['val_accuracy'], label="val_accuracy")
 plt.plot(history.history['loss'], label="loss")
 plt.plot(history.history['val_loss'], label="val_loss")
 plt.legend(loc='lower right')
 plt.show()
+
+
+# example of the model predicting on new, unseen data
+sunflower_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/592px-Red_sunflower.jpg"
+sunflower_path = tf.keras.utils.get_file('Red_sunflower', origin=sunflower_url)
+
+img = tf.keras.utils.load_img(
+    sunflower_path, target_size=(image_height, image_width)
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
