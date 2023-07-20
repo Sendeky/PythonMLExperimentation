152728ec7e26f976b53b9aecf8065a24f7f2f792
Sendeky <spruslasha@gmail.com>
2023-07-20 00:15:42 -0700

    added TF2-Image-Segmentation.py

---
+++ TF2-Image-Segmentation/TF2-Image-Segmentation.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+# use "pip install git+https://github.com/tensorflow/examples.git" to insyall tensorflow_examples
+from tensorflow_examples.models.pix2pix import pix2pix
+
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+
